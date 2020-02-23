c6506c76467260a9fdb9062c4da42782753881c9
Tim Donoghue <tmd112593@gmail.com>
2020-02-23 04:18:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 
 def my_each(collection)
   index = 0 
-  while (index < collection.length)
-    yeild 
+  while (index < collection.length) do 
+    yeild collection[index] 
+    index += 1 
  end
-
 
 end
